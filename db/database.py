7cbe7732d33c10afe8e5f8c22b26948464d7deea
Haker <noyemkaren@gmail.com>
2026-02-08 17:47:59 +0300

    fid

--- db/database.py
+++ db/database.py
@@ -8,6 +8,6 @@ engine = create_engine(DB_URL, echo=False)
 def init_db():
     SQLModel.metadata.create_all(engine)
 
-def get_session() -> Generator[Session]:
+def get_session() -> Generator[Session, None, None]:
     with Session(engine) as session:
         yield session
