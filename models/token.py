c71936e97aa156a5b60a30e06443aaab9d0ad916
Haker <noyemkaren@gmail.com>
2026-02-15 16:44:04 +0300

    fix: db

--- models/token.py
+++ models/token.py
@@ -4,5 +4,5 @@ from sqlmodel import SQLModel, Field
 
 class Token(SQLModel, table=True):
     token: str = Field(primary_key=True)
-    user_id: int = Field(foreign_key="User.id")
+    user_id: int = Field(foreign_key="users.id")
     created_at: datetime = Field(default_factory=datetime.now)
